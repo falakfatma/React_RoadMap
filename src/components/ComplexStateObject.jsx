6b0e591c418a75fe9345ad388ebcc878ac77bd93
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-08-23 20:13:00 +0000

    toggle icon on click

--- src/components/ComplexStateObject.jsx
+++ src/components/ComplexStateObject.jsx
@@ -8,22 +8,36 @@ export default function Objects() {
     profileImage: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359554_1280.png",
     starImageEmpty: "",
     starImageFilled: "",
-    isFavourate: false
+    isFavourate: true
   })
-  let setImage = contact.isFavourate ? contact.starImageFilled : contact.starImageEmpty;
+
+  let setImage = contact.isFavourate ? "https://img.freepik.com/premium-vector/star-icon-white-background_212417-976.jpg?w=2000" : "https://www.freeiconspng.com/thumbs/white-star-icon/white-star-icon-11.png";
+  // +++++++If Else Also Write This ++++++++++ 
+
   // if (contact.isFavourate) {
   //   setImage = contact.starImageFilled
   // } else {
   //   setImage = contact.starImageEmpty
   // }
 
+  function toggleImage() {
+    setContact(
+      preVal => {
+        return {
+          ...preVal,
+          isFavourate: !preVal.isFavourate
+        }
+      })
+  }
+
+  console.log(contact.isFavourate)
   return (
-    <article className="p-4 bg-white m-3 rounded">
+    <article className="p-4 text-m bg-white m-3 rounded w-56 h-96">
       <img src={contact.profileImage} className="h-48" />
       <div>
-        <img src={setImage} className="h-10" />
+        <img src={setImage} className="h-10" onClick={toggleImage} />
         <h1 className="text-3xl font-bold">{contact.name}</h1>
-        <h2 className="text-gray-600">{contact.number}</h2>
+        <h2 className="text-gray-600 mb-2 mt-2">{contact.number}</h2>
         <h2 className="text-gray-600">{contact.email}</h2>
       </div>
     </article>
